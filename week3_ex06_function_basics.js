354c884fbdab77b974e7b4546e4c20e95b63d8f1
Nonhlanhla Kunene <knonhlanhla585@gmail.com>
2026-06-13 14:27:20 +0200

    Add Exercise 06 - Function Basics

---
+++ week3_ex06_function_basics.js
@@ -0,0 +1,21 @@
+// TODO: Create a function called greet that takes a name parameter
+function greet(name) {
+
+// TODO: Make the greet function return "Hello, [name]!"  
+  return "Hello, "+ name +"!"
+}
+
+// TODO: Create a function called add that takes two parameters
+function add(a, b) {
+
+// TODO: Make the add function return the sum of both parameters  
+  return (a+b);
+}
+
+// TODO: Call both functions and store their results in variables
+let name = greet("Nonhlanhla");
+let sum = add(4, 7);
+
+// TODO: Print the results to the console
+console.log(name);
+console.log("The sum is:", sum);
