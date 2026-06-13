ae4b00776019c88dfcbc13941da2907984cd7362
Nonhlanhla Kunene <knonhlanhla585@gmail.com>
2026-06-13 14:20:02 +0200

    Add Exercise 04 - Conditional Logic

---
+++ week3_ex04_conditional_logic.js
@@ -0,0 +1,14 @@
+let score = 69;
+let passingGrade = 60;
+
+
+if (score==100) {
+  console.log("Perfect!");
+
+} else if (score>=passingGrade) {
+  console.log("Passed.");
+  
+} else {
+  console.log("Failed.");
+  
+}
