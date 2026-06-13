75aa0d92a003a9206dbf3a24eed102ebb501de29
Nonhlanhla Kunene <knonhlanhla585@gmail.com>
2026-06-13 14:17:39 +0200

    Add Exercise 03 - Array Basics

---
+++ week3_ex03_array_basics.js
@@ -0,0 +1,13 @@
+const colors = [];
+
+colors.push('Red', 'Orange', 'Green'); // push adds colors 
+
+const numbers = [1, 4, 8, 7, 5];
+
+
+colors.pop(); //pop removes last array
+colors.unshift("Purple"); // unshift adds new color to beginning
+
+
+console.log("The length of the colors array is", colors.length);
+console.log("The length of the numbers array is", numbers.length);
