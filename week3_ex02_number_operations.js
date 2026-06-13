e0b7a8c83fa7192a27770f4b782bf0fca366d46a
Nonhlanhla Kunene <knonhlanhla585@gmail.com>
2026-06-13 14:13:50 +0200

    Add Exercise 02 - Number Operations and Type Conversion

---
+++ week3_ex02_number_operations.js
@@ -0,0 +1,10 @@
+let number1 = 12;
+let number2 = 17;
+
+let string1 = number1.toString();
+
+let sum = number1 + number2 ;
+let product = number1*number2
+
+console.log("The sum is " +sum)
+console.log("The product is " + product)
